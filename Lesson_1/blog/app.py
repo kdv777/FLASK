f442769b3ab5bae7d7985d2cd20f7bb38e5db8ef
kdv777 <k-d@bk.ru>
2023-03-21 20:17:24 +0500

    lesson_1_1

--- Lesson_1/blog/app.py
+++ Lesson_1/blog/app.py
@@ -29,3 +29,13 @@ def process_after_request(response):
     return response
 
 
+@app.route("/divide-by-zero/")
+def do_zero_division():
+    return 1 / 0
+
+
+@app.errorhandler(ZeroDivisionError)
+def handle_zero_division_error(error):
+    print(error) # prints str version of error: 'division by zero'
+    app.logger.exception("Here's traceback for zero division error")
+    return "Never divide by zero!", 400
