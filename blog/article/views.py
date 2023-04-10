6207b5f5dd655209b261b2fca9d156212d293209
kdv777 <k-d@bk.ru>
2023-04-10 23:18:14 +0500

    lesson_6_finish

--- blog/article/views.py
+++ blog/article/views.py
@@ -1,5 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+from flask_login import current_user
 from werkzeug.exceptions import NotFound
+
+from blog.database import db
+from blog.forms.article import CreateArticleForm
+from blog.mymodels import Article, Author
 
 article = Blueprint('article', __name__, url_prefix='/articles', static_folder='../static')
 
@@ -22,18 +27,46 @@ ARTICLES = {
 }
 
 
-@article.route('/')
+@article.route('/', methods=["GET"])
 def article_list():
-    return render_template('articles/list.html', articles=ARTICLES)
+    articles = Article.query.all()
+    return render_template('articles/list.html', articles=articles)
 
 
 @article.route('/<int:pk>')
 def get_article(pk: int):
     try:
-        article_name = ARTICLES[pk]
+        _article = Article.query.filter_by(id=pk).one_or_none()
     except KeyError:
         raise NotFound(f'Article id {pk} not found')
     return render_template(
         'articles/details.html',
-        article_name=article_name,
+        article_name=_article
     )
+
+
+@article.route('/create/', methods=['GET'])
+def create_article_form():
+    form = CreateArticleForm(request.form)
+    return render_template('articles/create.html', form=form)
+
+
+@article.route('/', methods=['POST'])
+def create_article():
+    form = CreateArticleForm(request.form)
+    if form.validate_on_submit():
+        _article = Article(title=form.title.data.strip(), text=form.text.data)
+        if current_user.author:
+            _article.author_id = current_user.author.id
+        else:
+            author = Author(user_id=current_user.id)
+            db.session.add(author)
+            db.session.flush()
+            _article.author_id = author.id
+
+        db.session.add(_article)
+        db.session.commit()
+
+        return redirect(url_for('article.article_detail', article_id=_article.id))
+
+    return render_template('articles/create.html', form=form)
--- blog/mymodels/__init__.py
+++ blog/mymodels/__init__.py
@@ -1,7 +1,9 @@
+from blog.mymodels.article import Article
 from blog.mymodels.author import Author
 from blog.mymodels.user import User
 
 __all__ = [
     "User",
     "Author",
+    "Article",
 ]
